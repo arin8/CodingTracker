a0708284a05aa2ec2f0fb2fb29ceb0e33dbf1fea
arin8 <146653910+arin8@users.noreply.github.com>
2024-06-11 09:54:17 +0200

    Updated Program.cs by removing a line

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -5,4 +5,4 @@ Console.WriteLine("Hello, C# Academy! one more modification");
 Console.WriteLine("Hello, C# Academy! one more modification");
 Console.WriteLine("Hello, C# Academy! one more modification");
 Console.WriteLine("Hello, C# Academy! one more modification");
-Console.WriteLine("Hello, C# Academy! one more modification");
+
